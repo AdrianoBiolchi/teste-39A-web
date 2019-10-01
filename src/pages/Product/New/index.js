404ac7c408f566e14b6ebcf92d83fe0fa6898d30
Adriano Biolchi <adrianobiolchi@outlook.com>
2019-10-01 17:17:33 -0300

    create product

--- src/pages/Product/New/index.js
+++ src/pages/Product/New/index.js
@@ -1,7 +1,50 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import { withRouter } from 'react-router-dom';
+import ScaleLoader from 'react-spinners/ScaleLoader';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 // import { Container } from './styles';
+import api from '../../../services/api'
 
-export default function New() {
-  return <h1>New</h1>;
+function New(props) {
+  const [product, setProduct] = useState({ name: '', 'description':'' });
+  const [loading, setLoading] = useState(false);
+
+  const saveProduct = (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const data = { name: product.name, description: product.description };
+    api.post('/products ', data)
+      .then((result) => {
+        setLoading(false);
+        props.history.push('/details/' + result.data.id)
+      }).catch((error) => setLoading(false));
+  };
+
+  const onChange = (e) => {
+    e.persist();
+    setProduct({...product, [e.target.name]: e.target.value});
+  }
+
+  return (
+    <div>
+   <Jumbotron>
+      <Form onSubmit={saveProduct}>
+        <Form.Group>
+          <Form.Label>Product Name</Form.Label>
+          <Form.Control type="text" name="name" id="name" placeholder="Enter product name" value={product.name} onChange={onChange} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Product Description</Form.Label>
+          <Form.Control as="textarea" name="description" id="description" rows="3" placeholder="Enter product description" value={product.description} onChange={onChange} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Save
+        </Button>
+      </Form>
+    </Jumbotron>
+  </div>
+  );
 }
+export default withRouter(New);
